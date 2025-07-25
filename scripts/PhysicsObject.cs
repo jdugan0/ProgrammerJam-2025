dbe0426dde35270e6c702e0d47326e3d2e32b5fd
jdugan0 <justin.d.dugan@gmail.com>
2025-07-25 19:32:24 -0400

    gravity

--- scripts/PhysicsObject.cs
+++ scripts/PhysicsObject.cs
@@ -3,4 +3,20 @@ using System;
 
 public partial class PhysicsObject : RigidBody2D
 {
+    [Export] bool gravity;
+    public override void _Process(double delta)
+    {
+        if (gravity)
+        {
+            if (((GameManager)GetTree().GetNodesInGroup("GameManager")[0]).GetMovementState() == Movement.MovementState.SIDE)
+            {
+                GravityScale = 5.0f;
+            }
+            else
+            {
+                GravityScale = 0.0f;
+            }
+        }
+    }
+
 }
