8bb2b5faf5c3fbbbc6ed3e72d3e9f06d211654fa
jdugan0 <justin.d.dugan@gmail.com>
2025-07-25 14:45:28 -0400

    structure

--- scripts/Movement.cs
+++ scripts/Movement.cs
@@ -3,12 +3,37 @@ using System;
 
 public partial class Movement : CharacterBody2D
 {
+    public enum MovementState
+    {
+        SIDE,
+        TOP
+    }
+    [Export] public MovementState movementState = MovementState.SIDE;
     public override void _Ready()
     {
-        
+
     }
 
     public override void _Process(double delta)
+    {
+        switch (movementState)
+        {
+            case MovementState.SIDE:
+                MovementStateSide(delta);
+                break;
+            case MovementState.TOP:
+                MovementStateTop(delta);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void MovementStateSide(double delta)
+    {
+
+    }
+    public void MovementStateTop(double delta)
     {
         
     }
