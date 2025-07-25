35fa61b9b7f36bf82a0d46e4f071164719484eb9
Bearindigo105 <m.muthusubhash@gmail.com>
2025-07-25 19:45:45 -0400

    sounds

--- scripts/Audio/Sound.cs
+++ scripts/Audio/Sound.cs
@@ -2,7 +2,7 @@ using Godot;
 [GlobalClass]
 public partial class Sound : Resource
 {
-    [Export] public AudioStream stream { get; set; }
-    [Export] public float volume { get; set; }
-    [Export] public string name { get; set; }
+	[Export] public AudioStream stream { get; set; }
+	[Export] public float volume { get; set; }
+	[Export] public string name { get; set; }
 }
