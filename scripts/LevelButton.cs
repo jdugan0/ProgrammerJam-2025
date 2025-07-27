273fa438bfb7785247cde0ded652a09ead04696f
Bearindigo105 <m.muthusubhash@gmail.com>
2025-07-26 22:08:17 -0400

    level 1 fr fr!

--- scripts/LevelButton.cs
+++ scripts/LevelButton.cs
@@ -3,19 +3,19 @@ using System;
 
 public partial class LevelButton : TextureButton
 {
-    [Export] public int levelID = 1;
-    [Export] public Level_UI level_UI;
-    public override void _Ready()
-    {
-        if (GameManager.instance.levelUnlocked < levelID)
-        {
-            Disabled = true;
-        }
-    }
+	[Export] public int levelID = 1;
+	[Export] public Level_UI level_UI;
+	public override void _Ready()
+	{
+		if (GameManager.instance.levelUnlocked < levelID)
+		{
+			Disabled = true;
+		}
+	}
 
-    public void OnPress()
-    {
-        level_UI.ChooseLevel(levelID);
-    }
+	public void OnPress()
+	{
+		level_UI.ChooseLevel(levelID);
+	}
 
 }
