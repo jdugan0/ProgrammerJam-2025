40ab213af0b23195977b23ffe483461df211fbb4
jdugan0 <justin.d.dugan@gmail.com>
2025-07-26 23:52:46 -0400

    fixy fix

--- scripts/ScrollTexture.cs
+++ scripts/ScrollTexture.cs
@@ -15,7 +15,7 @@ public partial class ScrollTexture : Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		RegionRect = new Rect2(player.Position.X * parallax, player.Position.Y, 1920, 1080);
+		RegionRect = new Rect2(player.Position.X * parallax, player.Position.Y * parallax, 1920, 1080);
 		Position = new Vector2(player.Position.X, player.Position.Y);
 	}
 }
