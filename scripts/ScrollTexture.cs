9238db8b24047b6ec18e28b96a8bdd972c193b08
jdugan0 <justin.d.dugan@gmail.com>
2025-07-26 23:50:01 -0400

    fixed

--- scripts/ScrollTexture.cs
+++ scripts/ScrollTexture.cs
@@ -10,7 +10,7 @@ public partial class ScrollTexture : Sprite2D
 	public override void _Ready()
 	{
 	}
-	
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
