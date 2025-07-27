a284b4baad3dd06a3b9ae27225df7fea4252f8e1
jdugan0 <justin.d.dugan@gmail.com>
2025-07-27 11:18:46 -0400

    level 2 fixed

--- scripts/PhysicsObject.cs
+++ scripts/PhysicsObject.cs
@@ -4,5 +4,17 @@ using System;
 public partial class PhysicsObject : RigidBody2D
 {
     [Export] public bool gravity;
+    [Export] public float maxV = 100f;
+    public override void _Process(double delta)
+    {
+        if (gravity)
+        {
+            if (LinearVelocity.Length() > maxV)
+            {
+                LinearVelocity = LinearVelocity.Normalized() * maxV;
+            }
+        }
+    }
+
 
 }
