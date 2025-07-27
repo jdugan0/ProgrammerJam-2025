f387be1bb1d32032c3c65462a7c6bb16813949bd
1023941311414 <jake.widmann@outlook.com>
2025-07-26 21:00:13 -0400

    Level 2

---
+++ scripts/ScrollTexture.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class ScrollTexture : Sprite2D
+{
+	// Called when the node enters the scene tree for the first time.
+	[Export] float parallax;
+	[Export] CharacterBody2D player;
+
+	public override void _Ready()
+	{
+	}
+	
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		RegionRect = new Rect2(player.Position.X * parallax, player.Position.Y, 1920, 1080);
+		Position = new Vector2(player.Position.X, player.Position.Y);
+	}
+}
